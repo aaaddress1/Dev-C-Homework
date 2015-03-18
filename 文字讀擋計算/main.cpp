14fd297f26e2f9efff7530ad6dc16ccdfd085d7b
aaaddress1 <aaaddress1@gmail.com>
2015-03-19 01:48:15 +0800

    no message

---
+++ 文字讀擋計算/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+bool IsNumber(char i)
+{
+	return ((i > 47) && (i < 58));
+}
+bool IsUppercase(char i)
+{
+	return ((i > 64) && (i < 91));
+}
+bool IsLowercase(char i)
+{
+	return ((i > 96) && (i < 123));
+}
+bool IsSymbol(char i)
+{
+	return ((i > 31) && (i < 127)) && (!IsNumber(i)) && (!IsUppercase(i)) && (!IsLowercase(i));
+}
+int main()
+{
+	fstream fileInput;
+	fileInput.open("data.txt",ios::in);
+	if (!fileInput) exit(0);
+	int NumberCount(0),UpperCount(0),LowerCount(0),Symbol(0);
+	cout << "檔案內文字: ";
+	for(char current_char;fileInput.get(current_char);)
+	{
+		NumberCount += IsNumber(current_char);
+		UpperCount += IsUppercase(current_char);
+		LowerCount += IsLowercase(current_char);
+		Symbol += IsSymbol(current_char);	
+		cout <<current_char;
+	}
+	cout << "\n\n發現了一共 " << NumberCount << "個數字, " <<  UpperCount << "個大寫字, " ;
+	cout << LowerCount << "個小寫字, " << Symbol << "個符號,共" << (UpperCount+LowerCount) << "個單字.\n"; 
+	fileInput.close();
+	system("pause");
+	return 0;
+}
+
