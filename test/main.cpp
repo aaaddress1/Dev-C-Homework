51f3387eccc2456415382fda3d6dfbaf40164eb0
aaaddress1 <aaaddress1@gmail.com>
2015-05-11 20:42:30 +0800

    no message

--- test/main.cpp
+++ test/main.cpp
@@ -1,47 +1,50 @@
 #include <iostream>
-#include <fstream>
 #include <cstdlib>
-#include <ctime>
 using namespace std;
 
 
-struct date1
+class Car
 {
-   string id;
-   bool sex;
-   float weight;
-   float height;
-   float BMI;
-};
 
+private:
+	float a;
+	int b;
+	int c;
+	string d;
+	
+public:
+	float GetA()
+	{
+		return a;
+	}
+	
+	void SetA(int nA)
+	{
+		a = nA;
+	}
+	
+	Car()
+	{
+		a = 50;
+		b = 30;
+		c = 4;
+		d = "adr";
+	}
+	
+	Car(float inputa,int inputb,int inputc,string inputd)
+	{
+		a = inputa;
+		b = inputb;
+		c = inputc;
+		d = inputd;
+	} 
+};
 
 int main()
 {	
-	ifstream fin("data.txt");    
-	
-	date1 my_data;
-	int boy = 0;
-	int girl = 0;
-	
-  	cout << "編碼:   性別:   體重:   身高:   BMI:" << endl; 
-	while (fin>>my_data.id >> my_data.sex >> my_data.weight >> my_data.height)
-	{
-		
-		my_data.BMI = 10000*my_data.weight/my_data.height/my_data.height;
-		if (my_data.sex) 
-		{
-			boy++;	
-			cout << my_data.id << "\t男\t"<< my_data.weight << "\t" << my_data.height <<"\t" << my_data.BMI <<"\n";
+	Car n[3] = {Car(123,456,789,"adr1") , Car(10,11,12,"adr2"), Car(11,23,55,"adr3")};
 
-		}
-		else
-		{
-			girl++;
-			cout << my_data.id << "\t女\t"<< my_data.weight << "\t" << my_data.height <<"\t" << my_data.BMI <<"\n";
-		}
-	    
-    }
-	fin.close();
+
 	system("pause");
 	return 0;
 }
